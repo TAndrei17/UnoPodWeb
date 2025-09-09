f4b82df2766bf4420a1c3b2b771c0cbd0e10a7cb
Andrei Trunkin <andrei.trunkin@galvintec.com>
2025-09-09 12:00:19 +0200

    chore: update test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,8 @@
 import request from 'supertest';
+
 import app from '../src/app.js';
 
-test('request', async () => {
-	const res = await request(app).get('/');
-	if (res.error) {
-		throw res.error;
-	}
+test('podcastsTop', async () => {
+	const res = await request(app).get('/podcastsTop/country/es/limit/5');
 	expect(res.status).toBe(200);
 });
